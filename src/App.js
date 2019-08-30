00f2cd41f0a47716bfefb0ce7ea951360f5cd056
Shetty <roshan.shetty@igt.com>
2019-08-30 16:14:28 +1000

    added changes to working file before heading off

--- src/App.js
+++ src/App.js
@@ -6,19 +6,24 @@ import SearchBox from './SearchBox';
 class App extends Component {
 
     constructor(){
-        super();
+        super()
         this.state = {
             robots: robots,
             searchfield : ''
         }
     }
 
+    onSearchChange = (event) => {
+        console.log(event.target.value);
+
+    }
+
     render() {
         return (
             <div className='tc'>
                 <h1> Robo Friends  </h1>
-                <SearchBox />
-                <CardList robots={robots} />
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={this.state.robots} />
             </div>
         );
     }
